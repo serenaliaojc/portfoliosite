973811bb161724ad4f98f519b94ea378a9345124
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-05-17 08:37:39 -0400

    add index_intro

---
+++ static/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Serena Liao</title>
+</head>
+<body>
+	<div class="container">
+
+		<div class="index">
+			
+			<header>
+
+				<h1 hidden="hidden">Serena Liao's Portfolio</h1>
+
+				<img src="img/logo-blue.png" alt="logo">
+
+				<div class="intro">
+					<p>hi, I am <span>Serena</span>.</p>
+					<p>I am a full-stack web developer.</p>
+				</div>
+
+				<nav class="main-nav">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Works</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+
+			</header>
+
+			<main>
+				<div class="about">
+					<h2>About.</h2>
+					<div class="row">
+						<figure class="col-md-4">
+							<img src="" alt="serena's photo">
+							<figcaption>Serena Liao</figcaption>
+						</figure>
+						<div class="col-md-8">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam voluptatibus ea, reiciendis aut nisi voluptates deleniti pariatur, quas qui tempore perspiciatis odio assumenda, possimus a maiores magnam necessitatibus ipsa praesentium tenetur. Recusandae provident sunt in quidem, explicabo veniam, quibusdam amet rerum. Nemo atque sit nobis recusandae architecto iure, voluptas iusto.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo vitae quod enim alias optio sequi quae nihil sunt dolores in?</p>
+						</div>
+					</div>					
+				</div>
+			</main>
+
+			<footer></footer>
+
+		</div>
+
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</body>
+</html>
