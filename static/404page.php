49451cdbd51d82dd1e2496a24e0a996d2fb9e559
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-05-22 15:13:15 -0400

    add custom 404 page

---
+++ static/404page.php
@@ -0,0 +1,12 @@
+<?php include_once 'parts/header.php'; ?>
+<?php include_once 'parts/header-page.php'; ?>
+
+<div class="error page">
+	<h1>Sorry, 404 page not found.</h1>
+	<p>
+		Sorry, the page you are searching cannot be found.
+	</p>
+	<button><a href="index.php">Return to Home</a></button>
+</div>
+
+<?php include_once 'parts/footer.php'; ?>
---
+++ static/project-single-pam.php
@@ -0,0 +1,55 @@
+<?php 
+$page_title = 'Project Single - Serena Liao';
+include_once 'parts/header.php'; 
+?>
+
+<?php include_once 'parts/header-page.php'; ?>
+
+<div class="project-single page">
+	<h1>One Project</h1>
+	<p class="page-intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas nisi natus unde, asperiores, sapiente eaque soluta suscipit, molestias sunt consequatur fugit reprehenderit quo saepe. Ullam.</p>
+
+	<div class="project-show">
+		<div id="project-carousel" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#project-carousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#project-carousel" data-slide-to="1"></li>
+		    <li data-target="#project-carousel" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="img/photo.jpg" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="img/works/pamschallenge.png" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="img/works/pamschallenge.png" alt="Third slide">
+		    </div>
+		  </div>
+		</div>
+
+		<button><a href="">View Live Site</a></button>
+	</div>
+
+	<div class="project-tech">
+		<h2>Technologies.</h2>
+		<div class="row">
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+			<div class="col-2"><img src="img/logo-blue.png" alt=""></div>
+		</div>
+	</div>
+
+	<div class="project-details">
+		<h2>Details.</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi inventore vitae quae sit nihil eveniet nostrum error nobis maiores, illo, facere. Nulla nihil atque odit dolor suscipit distinctio, dolorem reiciendis. Quisquam placeat voluptatum modi veniam sunt nam, ullam voluptatibus. Facilis ipsa odit eos! Nisi vel ut nulla ipsum quisquam sequi.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum fuga, laboriosam similique, quisquam veniam laborum repellendus laudantium itaque doloribus nam. Animi aperiam fugiat iusto ab labore tempore enim voluptatem tenetur, voluptas iure dolorum, eos laboriosam dolore veniam blanditiis, quisquam pariatur. Mollitia nesciunt sapiente, nostrum inventore eveniet, a, tenetur enim atque quia quae magni qui facere nam impedit nihil error, recusandae quod ipsum ipsam cum aliquam dolorem quibusdam incidunt non quo. Explicabo dolore doloribus commodi odio assumenda tempora, accusantium, repudiandae minus sed, excepturi obcaecati. Beatae, non magni amet odit esse voluptates aliquid, quos qui omnis laudantium doloribus debitis, vero voluptatem harum!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam consectetur cumque voluptate eum ad illo, laudantium eaque minus corporis! Quisquam dolore nostrum eum aperiam impedit eligendi placeat modi, in dignissimos vel magni nisi provident facere adipisci cumque voluptas laboriosam natus voluptatem architecto voluptates, quo rem id. Dolore atque illum fuga!</p>
+	</div>
+
+</div>
+<?php include_once 'parts/footer.php'; ?>
